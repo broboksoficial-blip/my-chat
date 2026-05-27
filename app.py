58b51b99263aaab7414336c379981e8398e5e08f
broboksoficial-blip <broboksoficial@gmail.com>
2026-05-27 19:56:09 +0300

    app.py

--- app.py
+++ app.py
@@ -28,3 +28,13 @@ def send():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Сайт работает 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
