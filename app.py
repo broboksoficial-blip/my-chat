4c781201d0b3d06f765820f40d383a927b544dd0
broboksoficial-blip <broboksoficial@gmail.com>
2026-05-28 13:14:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -560,6 +560,21 @@ function loginGoogle(){
 </div>
 
 </body>
+<script>
+function toggleTheme(){
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark");
+}
+</script>
 </html>
 """
 
