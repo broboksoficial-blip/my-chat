b420f8cc20450758ef9c6f356c0b433728bde03f
broboksoficial-blip <broboksoficial@gmail.com>
2026-06-02 14:28:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -298,6 +298,11 @@ def home():
         peer=None,
         my_id=user_id
     )
+    @app.route("/set-theme/<mode>")
+def set_theme(mode):
+    if mode in ["dark", "light"]:
+        session["theme"] = mode
+    return redirect("/")
 # ---------------- Settings ----------------
 @app.route("/settings")
 def settings():
